0793be30eed013665926916ea39e927338991be5
Lucas Sansberro <lucas_sansberro@outlook.com>
2022-06-28 11:58:25 -0300

    Agregado botón de eliminación individual

--- app.js
+++ app.js
@@ -1,153 +1,190 @@
 //Construimos la clase y una función para armonizar la introducción de los objetos dentro del array.
 //Por la misma razón, utilizamos variables.
 class TipoDeReparacion {
-	constructor (id, nombre, precio, tipoDeLibro){
-		this.id = parseInt(id)
-		this.nombre = nombre
-		this.precio = parseInt(precio)
-		this.tipoDeLibro = tipoDeLibro
-	}
+  constructor (id, nombre, precio, categoria){
+    this.id = parseInt(id)
+    this.nombre = nombre
+    this.precio = parseInt(precio)
+    this.categoria = categoria
+  }
 }
 
-function pushearArrayPrincipal (id, nombre, precio, tipoDeLibro){	
-	opcionesDeReparacion.push(new TipoDeReparacion(id,  nombre, precio, tipoDeLibro))
-}
-
-const restauracion = "Restauración de portada: $"
-const encolado = "Encolado de lomo: $"
-const limpieza = "Limpieza: $"
-const pegado = "Pegado de páginas: $"
-const tarlatana = "Reemplazo de tarlatana: $"
-const restauracionPaginas = "Restauración de páginas: $" 
-
-const opcionesDeReparacion = []
-pushearArrayPrincipal(1, restauracion, 600, "Rústica")
-pushearArrayPrincipal(2, encolado, 1200, "Rústica")
-pushearArrayPrincipal(3, limpieza, 300, "Rústica")
-pushearArrayPrincipal(4, pegado, 2000, "Rústica")
-pushearArrayPrincipal(5, restauracion, 2000, "Cartoné")
-pushearArrayPrincipal(6, encolado, 5000, "Cartoné")
-pushearArrayPrincipal(7, limpieza, 1500, "Cartoné")
-pushearArrayPrincipal(8, pegado, 7000, "Cartoné")
-pushearArrayPrincipal(9, restauracion, 15000, "Cuero")
-pushearArrayPrincipal(10, tarlatana, 10000, "Cuero")
-pushearArrayPrincipal(11, limpieza, 8000, "Cuero")
-pushearArrayPrincipal(12, restauracionPaginas, 9000, "Cuero")
+let opcionesDeReparacion = []
+opcionesDeReparacion.push(new TipoDeReparacion(1, "Restauración de portada: $", 600, "Rústica"))
+opcionesDeReparacion.push(new TipoDeReparacion(2, "Encolado de lomo: $", 1200, "Rústica"))
+opcionesDeReparacion.push(new TipoDeReparacion(3, "Limpieza: $", 300, "Rústica"))
+opcionesDeReparacion.push(new TipoDeReparacion(4, "Pegado de páginas: $", 2000, "Rústica"))
+opcionesDeReparacion.push(new TipoDeReparacion(5, "Restauración de portada: $", 2000, "Cartoné"))
+opcionesDeReparacion.push(new TipoDeReparacion(6, "Encolado de lomo: $", 5000, "Cartoné"))
+opcionesDeReparacion.push(new TipoDeReparacion(7, "Limpieza: $", 1500, "Cartoné"))
+opcionesDeReparacion.push(new TipoDeReparacion(8, "Pegado de páginas: $", 7000, "Cartoné"))
+opcionesDeReparacion.push(new TipoDeReparacion(9, "Restauración de portada: $", 15000, "Cuero"))
+opcionesDeReparacion.push(new TipoDeReparacion(10, "Reemplazo de tarlatana: $", 10000, "Cuero"))
+opcionesDeReparacion.push(new TipoDeReparacion(11, "Limpieza: $", 8000, "Cuero"))
+opcionesDeReparacion.push(new TipoDeReparacion(12, "Restauración de páginas: $", 9000, "Cuero"))
 
 //Cargamos el listado al DOM
 for (let i = 0; i < opcionesDeReparacion.length; i++) {
-	if (opcionesDeReparacion[i].tipoDeLibro == "Rústica"){
-		ulRustica.innerHTML += `<li class="list-group-item clarisimo"><button id="${opcionesDeReparacion[i].id}"
-		class="botonDeScript"> ${opcionesDeReparacion[i].nombre + opcionesDeReparacion[i].precio}</button></li>`
-	}
-	else if (opcionesDeReparacion[i].tipoDeLibro == "Cartoné"){
-		ulCartone.innerHTML += `<li class="list-group-item clarisimo"><button id="${opcionesDeReparacion[i].id}"
-		class="botonDeScript"> ${opcionesDeReparacion[i].nombre + opcionesDeReparacion[i].precio}</button></li>`
-	}
-	else if (opcionesDeReparacion[i].tipoDeLibro == "Cuero"){
-		ulCuero.innerHTML += `<li class="list-group-item clarisimo"><button id="${opcionesDeReparacion[i].id}"
-		class="botonDeScript"> ${opcionesDeReparacion[i].nombre + opcionesDeReparacion[i].precio}</button></li>`
-	}
+  if (opcionesDeReparacion[i].categoria == "Rústica"){
+    ulRustica.innerHTML += `<li class="list-group-item clarisimo d-flex justify-content-between">
+    ${opcionesDeReparacion[i].nombre + opcionesDeReparacion[i].precio}
+    <button id="${opcionesDeReparacion[i].id}"class="botonEliminador crema"> + </button></li>`
+  }
+  else if (opcionesDeReparacion[i].categoria == "Cartoné"){
+    ulCartone.innerHTML += `<li class="list-group-item clarisimo d-flex justify-content-between">
+    ${opcionesDeReparacion[i].nombre + opcionesDeReparacion[i].precio}
+    <button id="${opcionesDeReparacion[i].id}" class="botonEliminador crema"> + </button></li>`
+  }
+  else if (opcionesDeReparacion[i].categoria == "Cuero"){
+    ulCuero.innerHTML += `<li class="list-group-item clarisimo d-flex justify-content-between">
+    ${opcionesDeReparacion[i].nombre + opcionesDeReparacion[i].precio}
+    <button id="${opcionesDeReparacion[i].id}" class="botonEliminador crema"> + </button></li>`
+  }
 }
+
+//Recorremos cada componente del carrito y se lo agregamos a items para luego meterlo en listaCarrito
+  //Le pasamos una funcion para que lea el id del producto y lo elimine si le hacemos click al texto
+  //la idea es poner un boton al lado y cambiar al funcion a ese boton
+function mirror () {
+  if (carrito !=""){
+    let items = "";
+    carrito.forEach((item, i) => {
+      items += `<li class="d-flex justify-content-between"><p class="textoListaCarrito">${item.nombre} ${item.precio}</p>
+      <button class="botonEliminador crema"onclick="botonEliminador(${item.id},${item.precio})"> X </button></li>`;
+    })
+    listaCarrito.innerHTML = items;
+    textoPrecioFinal.innerHTML = `Precio final: $${precioFinal}`;
+    localStorage.setItem("Carrito", JSON.stringify(carrito));
+    localStorage.setItem("Precio Final", JSON.stringify(precioFinal));}
+  else { 
+    listaCarrito.innerHTML = carrito;
+    textoPrecioFinal.innerHTML = `Precio final: $${precioFinal}`;
+    localStorage.setItem("Carrito", JSON.stringify(carrito));
+    localStorage.setItem("Precio Final", JSON.stringify(precioFinal));
+  }
+};
 
 //Definición de elementos y carga mediante Storage si es que está disponible
-let carrito = JSON.parse(sessionStorage.getItem("Carrito"))
-let precioFinal = JSON.parse(sessionStorage.getItem("Precio Final"))
-if (carrito == null){
-	carrito = []
-	precioFinal = 0
+let carrito = JSON.parse(localStorage.getItem("Carrito"));
+let precioFinal = JSON.parse(localStorage.getItem("Precio Final"));
+if (carrito == null) {
+  carrito = [];
+  precioFinal = 0;
 }
 else {
-	listaCarrito.innerHTML = carrito.join("</br>")
-	textoPrecioFinal.innerHTML = `Precio final: $${precioFinal}`
+  mirror()
 }
 
-//Creamos una función que nos ahorrará repetir instrucciones cuando operemos sobre el DOM
-function mirror() {
-	listaCarrito.innerHTML = carrito.join("</br>")
-	textoPrecioFinal.innerHTML = `Precio final: $${precioFinal}`
-	sessionStorage.setItem("Carrito", JSON.stringify(carrito))
-	sessionStorage.setItem("Precio Final", JSON.stringify(precioFinal))
-}
+const botonEliminador = (id, precio) => {
+  let idx = carrito.findIndex(p => p.id==id);
+  let resta = carrito.find(p=>p.precio==precio)
+  let eliminarPorId = carrito.splice(idx,1);
+  precioFinal = precioFinal - resta.precio
+  mirror();
+};
 
 //Agregamos funcionalidad a los botones en el paso anterior
-for (const item of opcionesDeReparacion){
-	let eventos = document.getElementById(item.id)
-	eventos.addEventListener("click", function(){
-		if (carrito.length < 12){
-			carrito.push(item.nombre + item.precio)
-			precioFinal += item.precio
-			mirror()
-			}
-			else{
-				alert("Lo sentimos, solo aceptamos hasta 12 productos."
-				+ " Para compras mayoristas, por favor contáctenos a través de un correo electrónico")
-			}
-	})
+for (const item of opcionesDeReparacion) {
+  let eventos = document.getElementById(item.id);
+  eventos.addEventListener("click", function () {
+    if (carrito.length < 12) {
+      carrito.push(item);
+      precioFinal += item.precio;
+      mirror();
+    } else {
+      alert(
+        "Lo sentimos, solo aceptamos hasta 12 productos." +
+          " Para compras mayoristas, por favor contáctenos a través de un correo electrónico"
+      );
+    }
+  });
 }
 
 //Botón para eliminar último en lista
-botonEliminar.addEventListener("click", function(){
-	if (carrito !=""){
-		precioFinal = precioFinal - Number(carrito[carrito.length-1].match(/(\d+)/g))
-		carrito.pop()
-		mirror()
-	}
-})
+botonVaciador.addEventListener("click", function () {
+  if (carrito != "") {
+    carrito = []
+    precioFinal = 0
+    mirror()
+  }
+});
+
 
 //Botón para ordenar de menor a mayor
-botonOrdenador.addEventListener("click", function(){
-	carrito.sort((a, b) => (Number(a.match(/(\d+)/g)) - Number((b.match(/(\d+)/g)))))
-	listaCarrito.innerHTML = carrito.join("</br>")
-	sessionStorage.setItem("Carrito", JSON.stringify(carrito))
-})
+botonOrdenador.addEventListener("click", function () {
+  function comparar(a, b){
+    if (a.precio < b.precio){
+      return -1;
+      if (a.precio>b.precio){
+        return 1;
+      }
+      return 0
+    }
+  }
+  carrito.sort(comparar)
+  mirror()
+});
 
 //Evitamos que el botón de enviar refresque la página cuando lo presionamos
-let formulario = document.getElementById("contact-form")
-formulario.addEventListener("submit", function(e){
-	e.preventDefault ();
-})
+let formulario = document.getElementById("contact-form");
+formulario.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
 
 //Incluimos el localStorage en el botón de enviar para que recuerde los mails que el usuario ingresó
-botonDeEnvio.addEventListener("click", function(){
-	if (carrito != ""){
-		let correo = localStorage.getItem("Correo");
-		if (correo == null) {
-			localStorage.setItem("Correo", prompt("Ingrese el correo electrónico al cual quiere que le mandemos la información"));
-			correo = localStorage.getItem("Correo");
-			alert("Hemos enviado la información a "+ correo);
-			carrito = []
-			precioFinal = 0
-			mirror()
-		}
-		else {
-			let cambio = prompt("Enviaremos el método de pago al  correo que usted ha registrado\n"
-			+ correo + "\n¿Está de acuerdo o prefiere cambiar la dirección?\n\nPresione:\n"
-			+ "1: Para enviar la información a ese correo \n" + "2: Para cambiar la dirección")
-			while ( cambio != 1 && cambio != 2 ) {
-				alert("Por favor, ingrese una opción válida");
-				cambio = prompt("Enviaremos el método de pago al  correo que usted ha registrado\n"
-				+ correo + "\n¿Está de acuerdo o prefiere cambiar la dirección?\n\nPresione:\n"
-				+ "1: Para enviar la información a ese correo \n" + "2: Para cambiar la dirección");
-			}
-			if(cambio == 1){
-				alert("Hemos enviado la información a " + correo);
-				carrito = []
-				precioFinal = 0
-				mirror()
-			}
-			else if (cambio == 2){
-				localStorage.setItem("Correo", prompt("Ingrese el correo electrónico al cual quiere que le mandemos la información"));
-				correo = localStorage.getItem("Correo");
-				alert("Hemos enviado la información a "+ correo);
-				carrito = []
-				precioFinal = 0
-				mirror()
-
-			}
-		}
-	}
-	else{
-		alert("Lo sentimos, parece que su carrito está vacío")
-	}
-})
+botonDeEnvio.addEventListener("click", function () {
+  if (carrito != "") {
+    let correo = localStorage.getItem("Correo");
+    if (correo == null) {
+      localStorage.setItem(
+        "Correo",
+        prompt(
+          "Ingrese el correo electrónico al cual quiere que le mandemos la información"
+        )
+      );
+      correo = localStorage.getItem("Correo");
+      alert("Hemos enviado la información a " + correo);
+      carrito = [];
+      precioFinal = 0;
+      mirror();
+    } else {
+      let cambio = prompt(
+        "Enviaremos el método de pago al  correo que usted ha registrado\n" +
+          correo +
+          "\n¿Está de acuerdo o prefiere cambiar la dirección?\n\nPresione:\n" +
+          "1: Para enviar la información a ese correo \n" +
+          "2: Para cambiar la dirección"
+      );
+      while (cambio != 1 && cambio != 2) {
+        alert("Por favor, ingrese una opción válida");
+        cambio = prompt(
+          "Enviaremos el método de pago al  correo que usted ha registrado\n" +
+            correo +
+            "\n¿Está de acuerdo o prefiere cambiar la dirección?\n\nPresione:\n" +
+            "1: Para enviar la información a ese correo \n" +
+            "2: Para cambiar la dirección"
+        );
+      }
+      if (cambio == 1) {
+        alert("Hemos enviado la información a " + correo);
+        carrito = [];
+        precioFinal = 0;
+        mirror();
+      } else if (cambio == 2) {
+        localStorage.setItem(
+          "Correo",
+          prompt(
+            "Ingrese el correo electrónico al cual quiere que le mandemos la información"
+          )
+        );
+        correo = localStorage.getItem("Correo");
+        alert("Hemos enviado la información a " + correo);
+        carrito = [];
+        precioFinal = 0;
+        mirror();
+      }
+    }
+  } else {
+    alert("Lo sentimos, parece que su carrito está vacío");
+  }
+});
